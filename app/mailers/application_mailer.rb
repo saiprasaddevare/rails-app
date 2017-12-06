3c8887f43ac25b6e4b12c68501cd1e6003e4bebb
saiprasaddevare <saiprasaddevare12@gmail.com>
2017-12-06 16:02:32 +0530

    Sample Rails app

---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: "'sai' <saiprasaddevare12@gmail.com>"
+  layout 'mailer'
+end
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,6 @@
+# Be sure to restart your server when you modify this file.
+
+# ApplicationController.renderer.defaults.merge!(
+#   http_host: 'nameless-savannah-89652.herokuapp.com',
+#   https: false
+# )
